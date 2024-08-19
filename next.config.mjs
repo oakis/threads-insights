7e683b2bdb22f7a33a40da962696d6bdc6e4e4e4
oakis <oakismen@gmail.com>
2024-08-19 22:28:20 +0200

    update build stuff

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/threads-insights",
+  output: "export",
+  reactStrictMode: true,
+};
 
 export default nextConfig;
