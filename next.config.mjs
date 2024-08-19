b4c3bc55eb25eb59e22d244dbf917b6a33ff1304
oakis <oakismen@gmail.com>
2024-08-19 22:37:18 +0200

    disable rule

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   basePath: "/threads-insights",
   output: "export",
   reactStrictMode: true,
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 };
 
 export default nextConfig;
